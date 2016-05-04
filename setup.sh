827b6c70ed2476a7423ca9ee08f69a7c3bc26b7e
Björn Andersson <bandersson@pivotal.io>
2016-05-04 16:32:30 +0800

    Move rename newmachine out

--- setup.sh
+++ setup.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-if [ "$(hostname -s)" = "newmachine" ] ; then
-  echo "This seems to be a new machine, what is its name? "
-  read machine_name
-
-  if [ "$machine_name" != '' ] ; then
-    echo "Setting machine name to '$machine_name'"
-    sudo scutil --set ComputerName $machine_name
-    sudo scutil --set LocalHostName $machine_name
-    sudo scutil --set HostName $machine_name
-    sudo hostname $machine_name
-    sudo diskutil rename / $machine_name
-  fi
-fi
-
 laptop_folder=/tmp/laptop
 laptop_script=$laptop_folder/mac
 
